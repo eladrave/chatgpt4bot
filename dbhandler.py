1c696635578403784841d05163267b9ef6e2dbc3
Elad Rave <86946294+eladrave@users.noreply.github.com>
2023-03-18 06:01:32 +0000

    Added sqlite

--- dbhandler.py
+++ dbhandler.py
@@ -3,16 +3,16 @@ import json
 from datetime import datetime
 import mysql.connector
 import psycopg2
+import sqlite3
 from replit import db
 
 db_type = os.environ['DB_TYPE']
-db_user = os.environ['DB_USER']
-db_password = os.environ['DB_PASSWORD']
-db_host = os.environ['DB_HOST']
-db_name = os.environ['DB_NAME']
-db_port = os.environ['DB_PORT']
-
-# Additional helper functions for Replit DB
+db_user = os.environ.get('DB_USER')
+db_password = os.environ.get('DB_PASSWORD')
+db_host = os.environ.get('DB_HOST')
+db_name = os.environ.get('DB_NAME')
+db_port = os.environ.get('DB_PORT')
+db_path = os.environ.get('DB_PATH')
 
 def format_key(phone_number):
     return f"chat_{phone_number}"
@@ -30,9 +30,6 @@ def deserialize_message(message_json):
         "content": message["content"]
     }
 
-  
-# Updated functions
-
 def get_database_connection():
     if db_type == 'mysql':
         return mysql.connector.connect(
@@ -42,10 +39,12 @@ def get_database_connection():
         return psycopg2.connect(
             user=db_user, password=db_password, host=db_host, dbname=db_name, port=db_port
         )
+    elif db_type == 'sqlite':
+        return sqlite3.connect(db_path)
     elif db_type == 'replit':
         return None
     else:
-        raise ValueError("Invalid database type. Please set the 'DB_TYPE' environment variable to 'mysql', 'postgres', or 'replit'.")
+        raise ValueError("Invalid database type. Please set the 'DB_TYPE' environment variable to 'mysql', 'postgres', 'sqlite', or 'replit'.")
 
 def get_messages_from_db(phone_number):
     if db_type == 'replit':
@@ -64,8 +63,13 @@ def get_messages_from_db(phone_number):
         connection = get_database_connection()
         cursor = connection.cursor()
 
-        cursor.execute("SELECT `From`, `To`, Role, Message FROM chat WHERE `From`=%s OR `To`=%s ORDER BY DateTime;",
-                       (phone_number, phone_number))
+        if db_type == 'sqlite':
+            cursor.execute("SELECT `From`, `To`, Role, Message FROM chat WHERE `From`=? OR `To`=? ORDER BY DateTime;",
+                           (phone_number, phone_number))
+        else:
+            cursor.execute("SELECT `From`, `To`, Role, Message FROM chat WHERE `From`=%s OR `To`=%s ORDER BY DateTime;",
+                           (phone_number, phone_number))
+        
         result = cursor.fetchall()
 
         messages = [{"role": row[2], "content": row[3]} for row in result]
@@ -74,7 +78,6 @@ def get_messages_from_db(phone_number):
         connection.close()
 
         return messages
-
 
 def insert_message_to_db(from_number, to_number, role, message):
     if db_type == 'replit':
@@ -91,8 +94,35 @@ def insert_message_to_db(from_number, to_number, role, message):
         connection = get_database_connection()
         cursor = connection.cursor()
 
-        cursor.execute("INSERT INTO chat (DateTime, `From`, `To`, Role, Message) VALUES (NOW(), %s, %s, %s, %s);",
-                       (from_number, to_number, role, message))
+        if db_type == 'sqlite':
+            cursor.execute("INSERT INTO chat (DateTime, `From`, `To`, Role, Message) VALUES (datetime('now'), ?, ?, ?, ?);",
+                           (from_number, to_number, role, message))
+        else:
+            cursor.execute("INSERT INTO chat (DateTime, `From`, `To`, Role, Message) VALUES (NOW(), %s, %s, %s, %s);",
+                           (from_number, to
+
+def insert_message_to_db(from_number, to_number, role, message):
+    if db_type == 'replit':
+        key = format_key(from_number)
+        message_obj = {"role": role, "content": message, "timestamp": datetime.now().isoformat()}
+
+        if key in db:
+            messages = db[key]
+            messages.append(message_obj)
+            db[key] = messages
+        else:
+            db[key] = [message_obj]
+    else:
+        connection = get_database_connection()
+        cursor = connection.cursor()
+
+        if db_type == 'sqlite':
+            cursor.execute("INSERT INTO chat (DateTime, `From`, `To`, Role, Message) VALUES (datetime('now'), ?, ?, ?, ?);",
+                           (from_number, to_number, role, message))
+        else:
+            cursor.execute("INSERT INTO chat (DateTime, `From`, `To`, Role, Message) VALUES (NOW(), %s, %s, %s, %s);",
+                           (from_number, to_number, role, message))
+        
         connection.commit()
 
         cursor.close()
--- main.py
+++ main.py
@@ -5,47 +5,58 @@ from dotenv import load_dotenv
 from flask import Flask, request, jsonify
 from twilio.twiml.messaging_response import MessagingResponse
 from dbhandler import get_messages_from_db, insert_message_to_db
+import promptlayer
+
+promptlayer.api_key = os.environ['PROMPTLAYER_API_KEY']
+openai = promptlayer.openai
 
 load_dotenv()
 app = Flask(__name__)
+app.debug = True
 
 openai.api_key = os.environ['OPENAI_API_KEY']
 model = os.environ['OPENAI_API_MODEL']
 initial_prompt = os.environ['INITIAL_PROMPT']
 twilio_phone_number = os.environ['TWILIO_PHONE_NUMBER']
 
+
 @app.route('/wachat', methods=['POST'])
 def wachat():
-    user_phone = request.form.get('From')
-    user_name = request.form.get('ProfileName')
-    user_message = request.form.get('Body')
+  user_phone = request.form.get('From')
+  user_name = request.form.get('ProfileName')
+  user_message = request.form.get('Body')
 
-    messages = get_messages_from_db(user_phone)
-    if not messages:
-        messages = [
-            {"role": "system", "content": initial_prompt},
-            {"role": "user", "content": ""},
-        ]
+  messages = get_messages_from_db(user_phone)
+  if not messages:
+    messages = [
+      {
+        "role": "system",
+        "content": initial_prompt
+      },
+      {
+        "role": "user",
+        "content": ""
+      },
+    ]
 
-    # Remove the 'timestamp' field from messages before sending to the API
-    for message in messages:
-        message.pop('timestamp', None)
-    insert_message_to_db(user_phone, twilio_phone_number, "user", user_message)
+  # Remove the 'timestamp' field from messages before sending to the API
+  for message in messages:
+    message.pop('timestamp', None)
+  insert_message_to_db(user_phone, twilio_phone_number, "user", user_message)
 
-    messages = [{k: v for k, v in msg.items()} for msg in messages]
+  messages = [{k: v for k, v in msg.items()} for msg in messages]
 
-    response = openai.ChatCompletion.create(
-        model=model,
-        messages=messages
-    )
+  response = openai.ChatCompletion.create(model=model, messages=messages)
 
-    assistant_message = response.choices[0].message.content
-    insert_message_to_db(twilio_phone_number, user_phone, "assistant", assistant_message)
+  assistant_message = response.choices[0].message.content
+  insert_message_to_db(twilio_phone_number, user_phone, "assistant",
+                       assistant_message)
 
-    twilio_response = MessagingResponse()
-    twilio_response.message(assistant_message)
+  twilio_response = MessagingResponse()
+  twilio_response.message(assistant_message)
 
-    return str(twilio_response)
+  return str(twilio_response)
+
 
 if __name__ == '__main__':
-    app.run("0.0.0.0")
+  app.run("0.0.0.0")
