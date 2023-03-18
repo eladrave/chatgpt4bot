cf710d3684a37b52ad8e1adc1791b55fa1a037db
Elad Rave <86946294+eladrave@users.noreply.github.com>
2023-03-18 19:56:44 +0000

    Added support to Voice memo from Whatsapp

--- dbhandler.py
+++ dbhandler.py
@@ -14,95 +14,108 @@ db_name = os.environ.get('DB_NAME')
 db_port = os.environ.get('DB_PORT')
 db_path = os.environ.get('DB_PATH')
 
+
 def format_key(phone_number):
-    return f"chat_{phone_number}"
+  return f"chat_{phone_number}"
+
 
 def serialize_message(message):
-    return {
-        "role": message["role"],
-        "content": message["content"]
-    }
+  return {"role": message["role"], "content": message["content"]}
+
 
 def deserialize_message(message_json):
-    message = json.loads(message_json)
-    return {
-        "role": message["role"],
-        "content": message["content"]
-    }
+  message = json.loads(message_json)
+  return {"role": message["role"], "content": message["content"]}
+
 
 def get_database_connection():
-    if db_type == 'mysql':
-        return mysql.connector.connect(
-            user=db_user, password=db_password, host=db_host, database=db_name, port=db_port
-        )
-    elif db_type == 'postgres':
-        return psycopg2.connect(
-            user=db_user, password=db_password, host=db_host, dbname=db_name, port=db_port
-        )
-    elif db_type == 'sqlite':
-        return sqlite3.connect(db_path)
-    elif db_type == 'replit':
-        return None
-    else:
-        raise ValueError("Invalid database type. Please set the 'DB_TYPE' environment variable to 'mysql', 'postgres', 'sqlite', or 'replit'.")
+  if db_type == 'mysql':
+    return mysql.connector.connect(user=db_user,
+                                   password=db_password,
+                                   host=db_host,
+                                   database=db_name,
+                                   port=db_port)
+  elif db_type == 'postgres':
+    return psycopg2.connect(user=db_user,
+                            password=db_password,
+                            host=db_host,
+                            dbname=db_name,
+                            port=db_port)
+  elif db_type == 'sqlite':
+    return sqlite3.connect(db_path)
+  elif db_type == 'replit':
+    return None
+  else:
+    raise ValueError(
+      "Invalid database type. Please set the 'DB_TYPE' environment variable to 'mysql', 'postgres', 'sqlite', or 'replit'."
+    )
+
 
 def get_messages_from_db(phone_number):
-    if db_type == 'replit':
-        key = format_key(phone_number)
-        if key in db:
-            messages = []
-            for message in db[key]:
-                if isinstance(message, str):
-                    messages.append(json.loads(message))
-                elif isinstance(message, dict):
-                    messages.append(dict(message))
-            return messages
-        else:
-            return []
+  if db_type == 'replit':
+    key = format_key(phone_number)
+    if key in db:
+      messages = []
+      for message in db[key]:
+        if isinstance(message, str):
+          messages.append(json.loads(message))
+        elif isinstance(message, dict):
+          messages.append(dict(message))
+      return messages
     else:
-        connection = get_database_connection()
-        cursor = connection.cursor()
+      return []
+  else:
+    connection = get_database_connection()
+    cursor = connection.cursor()
 
-        if db_type == 'sqlite':
-            cursor.execute("SELECT `From`, `To`, Role, Message FROM chat WHERE `From`=? OR `To`=? ORDER BY DateTime;",
-                           (phone_number, phone_number))
-        else:
-            cursor.execute("SELECT `From`, `To`, Role, Message FROM chat WHERE `From`=%s OR `To`=%s ORDER BY DateTime;",
-                           (phone_number, phone_number))
-        
-        result = cursor.fetchall()
+    if db_type == 'sqlite':
+      cursor.execute(
+        "SELECT `From`, `To`, Role, Message FROM chat WHERE `From`=? OR `To`=? ORDER BY DateTime;",
+        (phone_number, phone_number))
+    else:
+      cursor.execute(
+        "SELECT `From`, `To`, Role, Message FROM chat WHERE `From`=%s OR `To`=%s ORDER BY DateTime;",
+        (phone_number, phone_number))
 
-        messages = [{"role": row[2], "content": row[3]} for row in result]
+    result = cursor.fetchall()
 
-        cursor.close()
-        connection.close()
+    messages = [{"role": row[2], "content": row[3]} for row in result]
 
-        return messages
+    cursor.close()
+    connection.close()
+
+    return messages
+
 
 def insert_message_to_db(from_number, to_number, role, message):
-    if db_type == 'replit':
-        key = format_key(from_number)
-        message_obj = {"role": role, "content": message, "timestamp": datetime.now().isoformat()}
+  if db_type == 'replit':
+    key = format_key(from_number)
+    message_obj = {
+      "role": role,
+      "content": message,
+      "timestamp": datetime.now().isoformat()
+    }
 
-        if key in db:
-            messages = db[key]
-            messages.append(message_obj)
-            db[key] = messages
-        else:
-            db[key] = [message_obj]
+    if key in db:
+      messages = db[key]
+      messages.append(message_obj)
+      db[key] = messages
     else:
-        connection = get_database_connection()
-        cursor = connection.cursor()
+      db[key] = [message_obj]
+  else:
+    connection = get_database_connection()
+    cursor = connection.cursor()
 
-        if db_type == 'sqlite':
-            cursor.execute("INSERT INTO chat (DateTime, `From`, `To`, Role, Message) VALUES (datetime('now'), ?, ?, ?, ?);",
-                           (from_number, to_number, role, message))
-        else:
-            cursor.execute("INSERT INTO chat (DateTime, `From`, `To`, Role, Message) VALUES (NOW(), %s, %s, %s, %s);",
-                           (from_number, to_number, role, message))
-        
-        connection.commit()
+    if db_type == 'sqlite':
+      cursor.execute(
+        "INSERT INTO chat (DateTime, `From`, `To`, Role, Message) VALUES (datetime('now'), ?, ?, ?, ?);",
+        (from_number, to_number, role, message))
+    else:
+      cursor.execute(
+        "INSERT INTO chat (DateTime, `From`, `To`, Role, Message) VALUES (NOW(), %s, %s, %s, %s);",
+        (from_number, to_number, role, message))
 
-        cursor.close()
-        connection.close()
+    connection.commit()
 
+    cursor.close()
+    connection.close()
--- main.py
+++ main.py
@@ -6,6 +6,11 @@ from flask import Flask, request, jsonify
 from twilio.twiml.messaging_response import MessagingResponse
 from dbhandler import get_messages_from_db, insert_message_to_db
 import promptlayer
+import requests
+import io
+import tempfile
+from typing import Tuple
+import soundfile as sf
 
 promptlayer.api_key = os.environ['PROMPTLAYER_API_KEY']
 openai = promptlayer.openai
@@ -20,11 +25,59 @@ initial_prompt = os.environ['INITIAL_PROMPT']
 twilio_phone_number = os.environ['TWILIO_PHONE_NUMBER']
 
 
+def download_media(url: str) -> Tuple[str, io.BytesIO]:
+    response = requests.get(url)
+    response.raise_for_status()
+    content_disposition = response.headers.get('Content-Disposition', 'unknown')
+    file_name = content_disposition.split("filename=")[-1].strip('"') if "filename=" in content_disposition else "unknown"
+    content = io.BytesIO(response.content)
+    return (file_name, content)
+
+def handle_audio(url: str) -> str:
+    filename, audio_file = download_media(url)
+    
+    with tempfile.NamedTemporaryFile(delete=False, suffix=f".{filename.split('.')[-1]}") as temp_audio_file:
+        temp_audio_file.write(audio_file.read())
+        temp_audio_file.flush()
+
+        data, samplerate = sf.read(temp_audio_file.name)
+        with tempfile.NamedTemporaryFile(delete=False, suffix=".wav") as temp_wav_file:
+            sf.write(temp_wav_file.name, data, samplerate, format='WAV', subtype='PCM_16')
+            with open(temp_wav_file.name, 'rb') as wav_file:
+                response = openai.Audio.transcribe("whisper-1", wav_file)
+
+        os.unlink(temp_wav_file.name)
+    os.unlink(temp_audio_file.name)
+
+    print(response)
+    transcript = response['text']
+    return transcript
+
+def handle_image(url: str):
+  # Add your implementation to handle images
+  pass
+
+
+def handle_document(url: str):
+  # Add your implementation to handle documents
+  pass
+
+
 @app.route('/wachat', methods=['POST'])
 def wachat():
   user_phone = request.form.get('From')
   user_name = request.form.get('ProfileName')
   user_message = request.form.get('Body')
+  media_url = request.form.get('MediaUrl0')
+  media_type = request.form.get('MediaContentType0')
+
+  if media_url:
+    if media_type.startswith('audio'):
+      user_message = handle_audio(media_url)
+    elif media_type.startswith('image'):
+      handle_image(media_url)
+    elif media_type.startswith('application'):
+      handle_document(media_url)
 
   messages = get_messages_from_db(user_phone)
   if not len(messages):
